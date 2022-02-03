ecf4e5f04c28e2b57fe21e7ac35c5375f70f6eba
Kunal Kotwani 🚀 <kkotwani@amazon.com>
2022-02-02 23:10:49 -0800

    Updating config for personal repo

--- src/run_perf_test.py
+++ src/run_perf_test.py
@@ -22,8 +22,8 @@ from test_workflow.perf_test.perf_test_suite import PerfTestSuite
 
 def get_infra_repo_url():
     if "GITHUB_TOKEN" in os.environ:
-        return "https://${GITHUB_TOKEN}@github.com/opensearch-project/opensearch-infra.git"
-    return "https://github.com/opensearch-project/opensearch-infra.git"
+        return "https://${GITHUB_TOKEN}@github.com/kotwanikunal/opensearch-infra.git"
+    return "https://github.com/kotwanikunal/opensearch-infra.git"
 
 
 def main():
