04574dd91b7a3156d2ae3aecc7f010af8640f137
Kunal Kotwani 🚀 <kkotwani@amazon.com>
2022-02-11 00:04:35 -0800

    Parameterizing the script

--- vars/runPerfTestScript.groovy
+++ vars/runPerfTestScript.groovy
@@ -3,24 +3,18 @@ void call(Map args = [:]) {
     lib = library(identifier: 'jenkins@20211123', retriever: legacySCM(scm))
     def buildManifest = lib.jenkins.BuildManifest.new(readYaml(file: args.buildManifest))
     String artifactRootUrl = buildManifest.getArtifactRootUrl(jobName, args.buildId)
-    echo "Artifact root URL: ${artifactRootUrl}"
-    echo "${args}"
-    echo "${args.buildManifest}"
-    echo "${buildManifest}"
-
 
     install_npm()
     install_dependencies()
     withAWS(role: 'opensearch-test', roleAccount: "${AWS_ACCOUNT_PUBLIC}", duration: 900, roleSessionName: 'jenkins-session') {
         s3Download(file: "config.yml", bucket: "${ARTIFACT_BUCKET_NAME}", path: "${PERF_TEST_CONFIG_LOCATION}/config.yml", force: true)
-        s3Download(file: "manifest.yml", bucket: "${ARTIFACT_BUCKET_NAME}", path: "${PERF_TEST_MANIFEST_LOCATION}/manifest.yml", force: true)
     }
 
     sh([
         './test.sh',
         'perf-test',
-        "--stack test-single-disabled",
-        "--bundle-manifest manifest.yml",
+        "--stack test-single-disabled-${args.buildId}",
+        "--bundle-manifest ${args.buildManifest}",
         "--config config.yml"
 
     ].join(' '))
