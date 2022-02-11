5f9793410226d95e0305c2e85e1ced0798a61452
Kunal Kotwani 🚀 <kkotwani@amazon.com>
2022-02-10 22:19:01 -0800

    Enabling variables

--- vars/runPerfTestScript.groovy
+++ vars/runPerfTestScript.groovy
@@ -1,9 +1,13 @@
 void call(Map args = [:]) {
     String jobName = args.jobName ?: 'distribution-build-opensearch'
     lib = library(identifier: 'jenkins@20211123', retriever: legacySCM(scm))
-//     def buildManifest = lib.jenkins.BuildManifest.new(readYaml(file: args.buildManifest))
-//     String artifactRootUrl = buildManifest.getArtifactRootUrl(jobName, args.buildId)
-//     echo "Artifact root URL: ${artifactRootUrl}"
+    def buildManifest = lib.jenkins.BuildManifest.new(readYaml(file: args.buildManifest))
+    String artifactRootUrl = buildManifest.getArtifactRootUrl(jobName, args.buildId)
+    echo "Artifact root URL: ${artifactRootUrl}"
+    echo "${args}"
+    echo "${args.buildManifest}"
+    echo "${buildManifest}"
+
 
     install_npm()
     install_dependencies()
